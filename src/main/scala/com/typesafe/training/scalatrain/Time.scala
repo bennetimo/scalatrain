1e3368749f23c8c19b3adbeb5f5af7b7839e8e56
Tim Bennett <bennetimo@gmail.com>
2013-02-27 13:21:46 +0000

    add comment about using cons in the match expression

--- src/main/scala/com/typesafe/training/scalatrain/Time.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Time.scala
@@ -24,6 +24,12 @@ object Time {
   @tailrec
   def isIncreasing(times: Seq[Time]): Boolean = times match {
     //case t1 :: t2 :: rest => (t1 < t2) && isIncreasing(t2 :: rest)
+    /**
+     * The first case example above will pass all the unit tests, as they are written to pass in Seqs and Lists. However,
+     * it uses cons (::) to match, which is only defined on list, but not for example on Vector. That means if we pass in
+     * a Vector(Time()) then it won't match the first case as the type can't match, so it will hit the catch all. We could
+     * use case Seq() instead of case _ for the empty case to force a runtime match error if we were using the cons approach
+     */
     case t1 +: t2 +: rest => (t1 < t2) && isIncreasing(t2 +: rest)
     case _ => true
   }
